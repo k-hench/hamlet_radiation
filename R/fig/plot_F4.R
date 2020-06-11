aadab41ec9b9d092fa8c47c5c02ad5db528d0c98
k-hecnh <khench@posteo.net>
2020-06-11 17:48:28 +0200

    main figures are documented

--- R/fig/plot_F4.R
+++ R/fig/plot_F4.R
@@ -46,7 +46,7 @@ source(twisst_script)
 
 # start script -------------------
 # import fst data
-fst_data <- vroom::vroom(fst_file,delim = '\t') %>%
+fst_data <- vroom::vroom(fst_file, delim = '\t') %>%
   select(CHROM, BIN_START, BIN_END, N_VARIANTS, WEIGHTED_FST) %>%
   setNames(., nm = c('CHROM', 'BIN_START', 'BIN_END', 'n_snps', 'fst') ) %>%
   add_gpos() %>%
@@ -54,9 +54,10 @@ fst_data <- vroom::vroom(fst_file,delim = '\t') %>%
   setNames(., nm = c('GPOS','value')) %>%
   mutate(window = str_c('bold(',project_case('a'),'):joint~italic(F[ST])'))
 
-# import dxy data
+# locate dxy data files
 dxy_files <- dir(dxy_dir)
 
+# import dxy data
 dxy_data <-  str_c(dxy_dir,dxy_files) %>%
   purrr::map(get_dxy) %>%
   bind_rows() %>%
@@ -64,6 +65,7 @@ dxy_data <-  str_c(dxy_dir,dxy_files) %>%
   mutate(pop1 = str_sub(run,1,6),
          pop2 = str_sub(run,8,13))
 
+# compute delta dxy
 dxy_summary <- dxy_data %>%
   group_by(GPOS) %>%
   summarise(delta_dxy = max(dxy)-min(dxy),
@@ -77,13 +79,16 @@ dxy_summary <- dxy_data %>%
   gather(key = 'window', value = 'value',2:4) %>%
   filter(window == str_c('bold(',project_case('e'),'):\u0394~italic(d[xy])'))
 
-# import G x P data
+
+# set G x P traits to be imported
 traits <- c("Bars.lm.50k.5k.txt.gz", "Peduncle.lm.50k.5k.txt.gz", "Snout.lm.50k.5k.txt.gz")
 
+# set trait figure panels
 trait_panels <- c(Bars = str_c('bold(',project_case('h'),')'),
                   Peduncle = str_c('bold(',project_case('i'),')'),
                   Snout = str_c('bold(',project_case('j'),')'))
 
+# import G x P data
 gxp_data <- str_c(gxp_dir,traits) %>%
   purrr::map(get_gxp) %>%
   join_list() %>%
@@ -95,27 +100,37 @@ globals <- vroom::vroom(global_fst_file, delim = '\t',
   mutate(run = str_c(str_sub(run,1,3),loc,'-',str_sub(run,5,7),loc),
          run = fct_reorder(run,weighted))
 
-# select dxy data
+# dxy and pi are only shown for one exemplary population (/pair)
+# select dxy pair run (15 is one of the two central runs of the 28 pairs)
+# here, the 15th lowest fst value is identified as "selector"
 selectors_dxy <- globals %>%
   arrange(weighted) %>%
   .$weighted %>%
   .[15]
 
+# the dxy population pair corresponding to the selector is identified
 select_dxy_runs <- globals %>%
   filter(weighted %in% selectors_dxy) %>%
   .$run %>% as.character()
 
+# then thne dxy data is subset based on the selector
 dxy_select <- dxy_data %>%
   filter(run %in% select_dxy_runs) %>%
   mutate(window = str_c('bold(',project_case('b'),'): italic(d[XY])'))
 
-# select pi data
+# the pi data is filtered on a similar logic to the dxy data
+# first a table with the genome wide average pi for each population is compiled
+# (based on the first populations from the dxy data table
+# which contains pi for both populations)
 pi_summary_1 <- dxy_data %>%
   group_by(pop1,run) %>%
   summarise(avg_pi = mean(PI_POP1)) %>%
   ungroup() %>%
   set_names(., nm = c('pop','run','avg_pi'))
 
+# the mean genome wide average pi is compiled for all the second populations
+# from the dxy data
+# then, the average of all the comparisons is computed for each population
 pi_summary <- dxy_data %>%
   group_by(pop2,run) %>%
   summarise(avg_pi = mean(PI_POP2)) %>%
@@ -130,15 +145,22 @@ pi_summary <- dxy_data %>%
             sd_pi = sd(avg_pi)) %>%
   arrange(n)
 
+# one of the central populations with respect to average genome
+# wide pi is identified
+# for this, the 7th lowest pi value of the 14 populations is
+# determined as "selector"
 selectors_pi <- pi_summary %>%
   .$mean_pi %>%
   sort() %>%
   .[7]
 
+# the respective population is identified
 select_pi_pops <- pi_summary %>%
   filter(mean_pi %in% selectors_pi) %>%
-  .$pop %>% as.character
+  .$pop %>% as.character()
 
+# then the dxy data is subset by that population and the average pi over
+# all pair-wise runs is calculated for each window
 pi_data_select <- dxy_data %>%
   select(GPOS, PI_POP1, pop1 )%>%
   set_names(., nm = c('GPOS','pi','pop')) %>%
@@ -166,6 +188,7 @@ twisst_data <- tibble(loc = c('bel','hon'),
   bind_rows() %>%
   select(GPOS, topo3,topo_rel,window,weight)
 
+# the "null-weighting" is computed for both locations
 twisst_null <- tibble(window = c(str_c('bold(',project_case('f'),'):~italic(w)[bel]'),
                                  str_c('bold(',project_case('g'),'):~italic(w)[hon]')),
                       weight = c(1/15, 1/105))
@@ -176,8 +199,10 @@ data <- bind_rows(dxy_summary, fst_data, gxp_data)
 # import fst outliers
 outliers <-  vroom::vroom(outlier_table, delim = '\t')
 
+# the focal outlier IDs are set
 outlier_pick <- c('LG04_1', 'LG12_3', 'LG12_4')
 
+# the table for the outlier labels is created
 outlier_label <- outliers %>%
   filter(gid %in% outlier_pick) %>%
   mutate(label = letters[row_number()] %>% project_inv_case(),
@@ -186,48 +211,63 @@ outlier_label <- outliers %>%
          gpos_label2 = gpos_label - sign(x_shift_label) *.5*10^7,
          window = str_c('bold(',project_case('a'),'):joint~italic(F[ST])'))
 
+# the y height of the outlier labels and the corresponding tags is set
 outlier_y <- .45
 outlier_yend <- .475
 
+# the icons for the traits of the GxP are loaded
 trait_tibble <- tibble(window = c("bold(h):italic(p)[Bars]",
                                   "bold(i):italic(p)[Peduncle]",
                                   "bold(j):italic(p)[Snout]"),
                        grob = hypo_trait_img$grob_circle[hypo_trait_img$trait %in% c('Bars', 'Peduncle', 'Snout')])
 
-
-# trait_tibble$grob[[2]] <- trait_tibble$grob[[2]] %>% hypo_recolor_svg("white",layer = 4)
-# trait_tibble$grob[[1]] <- trait_tibble$grob[[1]] %>% reduce(.init = .,.f = hypo_recolor_svg, .x = c(4,5,6,7),color = "gray")
-
+# finally, the figure is being put together
 p_done <- ggplot()+
+  # the gray/white backgrounds of the LGs are added
   geom_hypo_LG()+
+  # the red highlights for the outlier regions are added
   geom_vline(data = outliers, aes(xintercept = gpos), color = outlr_clr)+
+  # the tags of the outlier labels are added
   geom_segment(data = outlier_label,
                aes(x = gpos,
                    xend = gpos_label2, y = outlier_y, yend = outlier_yend),
                color = alpha(outlr_clr,1),size = .2)+
+  # the outlier labels are added
   geom_text(data = outlier_label, aes(x = gpos_label, y = outlier_yend, label = label),
             color = alpha(outlr_clr,1), fontface = 'bold')+
+  # the fst, delta dxy and gxp data is plotted
   geom_point(data = data, aes(x = GPOS, y = value),size = plot_size, color = plot_clr) +
-  geom_point(data = dxy_select,aes(x= GPOS, y = dxy),size = plot_size, color = plot_clr)+
+  # the dxy data is plotted
+  geom_point(data = dxy_select,aes(x = GPOS, y = dxy),size = plot_size, color = plot_clr)+
+  # the pi data is plotted
   geom_point(data = pi_data_select, aes(x = GPOS, y = mean_pi),size = plot_size, color = plot_clr) +
+  # the roh data is plotted
   geom_point(data = recombination_data, aes(x = GPOS, y = RHO),size = plot_size, color = plot_clr) +
+  # the smoothed rho is plotted
   geom_smooth(data = recombination_data, aes(x = GPOS, y = RHO, group = CHROM),
                 color = 'red', se = FALSE, size = .7) +
+  # the topology weighting data is plotted
   geom_line(data = twisst_data, aes(x = GPOS, y = weight, color = topo_rel), size = .4) +
+  # the null weighting is added
   geom_hline(data = twisst_null, aes(yintercept = weight), color = rgb(1, 1, 1, .5), size = .4) +
+  # the trait icons are added
   geom_hypo_grob(data = trait_tibble,
                  aes(grob = grob, angle = 0, height = .65),
                  inherit.aes = FALSE, x = .95, y = 0.65)+
+  # setting the scales
   scale_fill_hypo_LG_bg() +
   scale_x_hypo_LG()+
   scale_color_gradient( low = "#f0a830ff", high = "#084082ff", guide = FALSE)+
+  # organizing the plot across panels
   facet_grid(window~.,scales = 'free',switch = 'y', labeller = label_parsed)+
+  # tweak plot appreance
   theme_hypo()+
   theme(legend.position = 'bottom',
     axis.title = element_blank(),strip.text = element_text(size = 11),
     strip.background = element_blank(),
     strip.placement = 'outside')
 
+# export figure 4
 scl <- .8
 hypo_save(p_done, filename = 'figures/F4.png',
           width = 297*scl, height = 275*scl,
--- R/fig/plot_F5.R
+++ R/fig/plot_F5.R
@@ -5,7 +5,7 @@
 #   https://raw.githubusercontent.com/simonhmartin/twisst/master/plot_twisst.R \
 #   2_analysis/summaries/fst_outliers_998.tsv 2_analysis/dxy/50k/ \
 #   2_analysis/fst/50k/ 2_analysis/summaries/fst_globals.txt \
-#   2_analysis/GxP/50000/ 200 5
+#   2_analysis/GxP/50000/ 200 5 2_analysis/fst/poptree/summary/
 # ===============================================================
 # This script produces Figure 5 of the study "Ancestral variation, hybridization and modularity
 # fuel a marine radiation" by Hench, McMillan and Puebla
@@ -35,7 +35,6 @@ plot_comment <- script_name %>%
   str_c('mother-script = ',getwd(),'/',.)
 
 args <- process_input(script_name, args)
-
 # config -----------------------
 w_path <- as.character(args[1])
 d_path <- as.character(args[2])
@@ -55,37 +54,46 @@ window_buffer <- 2.5*10^5
 #-------------------
 library(ape)
 library(igraph)
-
 # actual script =========================================================
-
+# locate dxy data files
 dxy_files <- dir(dxy_dir, pattern = str_c('dxy.*[a-z]{3}.*.', resolution ,'0kb-', resolution ,'kb.tsv.gz'))
 
+# import dxy data
 dxy_data <- tibble(file = str_c(dxy_dir, dxy_files)) %>%
   purrr::pmap_dfr(get_dxy, kb = str_c(resolution ,'0kb'))
 
+# set traits of interest for GxP
 gxp_traits <- c('Bars', 'Snout', 'Peduncle')
 
+# import GxP data
 gxp_data <- str_c(gxp_dir,gxp_traits,'.lm.', resolution ,'0k.', resolution ,'k.txt.gz') %>%
   future_map_dfr(get_gxp_long, kb = 50)
 
+# set topology weighting color scheme
 twisst_clr <- c(Blue = "#0140E5", Bars = "#E32210", Butter = "#E4E42E")
+# set GxP color scheme
 gxp_clr <- c(Bars = "#79009f", Snout = "#E48A00", Peduncle = "#5B9E2D") %>%
   darken(factor = .95) %>%
   set_names(., nm = gxp_traits)
 
+# copute genome wide average dxy
 dxy_globals <- dxy_data %>%
   filter(BIN_START %% ( resolution * 10000 ) == 1 ) %>%
   group_by( run ) %>%
   summarise(mean_global_dxy = sum(dxy*N_SITES)/sum(N_SITES)) %>%
   mutate(run = fct_reorder(run,mean_global_dxy))
 
+# import genome wide average fst
+# and order population pairs by genomewide average fst
 fst_globals <- vroom::vroom(fst_globals,delim = '\t',
                         col_names = c('loc','run_prep','mean_fst','weighted_fst')) %>%
   separate(run_prep,into = c('pop1','pop2'),sep = '-') %>%
   mutate(run = str_c(pop1,loc,'-',pop2,loc),
          run = fct_reorder(run,weighted_fst))
 
+# locate fst data files
 fst_files <- dir(fst_dir ,pattern = str_c('.', resolution ,'0k.windowed.weir.fst.gz'))
+# import fst data
 fst_data <- str_c(fst_dir, fst_files) %>%
   future_map_dfr(get_fst, kb = str_c(resolution ,'0k')) %>%
   left_join(dxy_globals) %>%
@@ -93,12 +101,14 @@ fst_data <- str_c(fst_dir, fst_files) %>%
   mutate(run = refactor(., fst_globals),
          BIN_MID = (BIN_START+BIN_END)/2)
 
+# order dxy population pairs by genomewide average fst
 dxy_data <- dxy_data %>%
   left_join(dxy_globals) %>%
   left_join(fst_globals) %>%
   mutate(run = refactor(dxy_data, fst_globals),
          window = 'bold(italic(d[xy]))')
 
+# compute delta dxy
 data_dxy_summary <- dxy_data %>%
   group_by(GPOS) %>%
   summarise(scaffold = CHROM[1],
@@ -113,48 +123,57 @@ data_dxy_summary <- dxy_data %>%
             delta_dxy = max(dxy)-min(dxy))
 
 # twisst part ------------------
+# load fst outlier regions
 outlier_table <- vroom::vroom(out_table, delim = '\t') %>%
   setNames(., nm = c("outlier_id","lg", "start", "end", "gstart","gend","gpos"))
 
+# set outlier regions of interest
 outlier_pick = c('LG04_1', 'LG12_3', 'LG12_4')
 
+# select genes to label
 cool_genes <-  c('arl3','kif16b','cdx1','hmcn2',
                  'sox10','smarca4',
                  'rorb',
                  'alox12b','egr1',
                  'ube4b','casz1',
-                 'hoxc8a','hoxc9','hoxc10a',#'hoxc6a',
-                 #'hoxc8a',
+                 'hoxc8a','hoxc9','hoxc10a',
                  'hoxc13a','rarga','rarg',
                  'snai1','fam83d','mafb','sws2abeta','sws2aalpha','sws2b','lws','grm8')
 
+# load twisst data
 data_tables <- list(bel = prep_data(loc = 'bel'),
                     hon = prep_data(loc = 'hon'))
 
-pops_bel <- c('ind','may','nig','pue','uni')
+# set species sampled in belize
+pops_bel <- c('ind', 'may', 'nig', 'pue', 'uni')
 
-neighbour_tibbles <- tibble(outlier_id = outlier_pick,
-                            #loc = c(rep('bel',3)),
+# set outlier region label
+region_label_tibbles <- tibble(outlier_id = outlier_pick,
                             label = c('A','B','C'))
 
+# load and recolor trait icons
 trait_grob <- tibble(svg = hypoimg::hypo_trait_img$grob_circle[hypoimg::hypo_trait_img$trait %in% gxp_traits],
                      layer = c(4,3,7),
                      color = gxp_clr[gxp_traits %>% sort()])%>%
   pmap(.f = hypo_recolor_svg) %>%
   set_names(nm = gxp_traits %>% sort())
 
+# recolor second bars-layer
 trait_grob[["Bars"]] <- trait_grob[["Bars"]] %>% hypo_recolor_svg(layer = 7,color = gxp_clr[["Bars"]])
 
-
+# prepare population tree trait_panels ----------------------
+# load outler region wide average fst data
 tree_list <- outlier_pick %>%
   purrr::map_dfr(get_fst_summary_data)
 
+# create neighbour-joining trees and plot
 poptree_plot_list <-  tree_list %>%
   purrr::pmap(plot_fst_poptree)
 
+# compose base figure
 p_single <- outlier_table %>%
   filter(outlier_id %in% outlier_pick) %>%
-  left_join(neighbour_tibbles) %>%
+  left_join(region_label_tibbles) %>%
   mutate(outlier_nr = row_number(),
          text = ifelse(outlier_nr == 1,TRUE,FALSE),
          trait = c('Snout', 'Bars', 'Peduncle')) %>%
@@ -164,15 +183,23 @@ p_single <- outlier_table %>%
                      rel_heights = c(1, .17),
                      labels = letters[1:length(outlier_pick)] %>% project_case())
 
+# compile legend
+# dummy plot for fst legend
 p_dummy_fst <- outlier_table %>% filter(row_number() == 1) %>% purrr::pmap(plot_panel_fst) %>% .[[1]]
+# dummy plot for  gxp legend
 p_dummy_gxp <- outlier_table %>% filter(row_number() == 1) %>% purrr::pmap(plot_panel_gxp, trait = 'Bars') %>% .[[1]]
+# fst legend
 p_leg_fst <- (p_dummy_fst+theme(legend.position = 'bottom')) %>% get_legend()
-p_leg_poptree <- (poptree_plot_list[[1]] + theme(legend.position = "bottom")) %>% get_legend()
+# gxp legend
 p_leg_gxp <- (p_dummy_gxp+theme(legend.position = 'bottom')) %>% get_legend()
+# create poptree legend
+p_leg_poptree <- (poptree_plot_list[[1]] + theme(legend.position = "bottom")) %>% get_legend()
+# create sub-legend 1
 p_leg1 <- cowplot::plot_grid(p_leg_fst,
                              p_leg_gxp,
                              ncol = 1)
 
+# create sub-legend 2 (phylogeny schematics)
 p_leg2 <- tibble(spec1 = c('indigo', 'indigo','unicolor'),
                  spec2 = c('maya', 'puella',NA),
                  color = twisst_clr %>% unname() %>% darken(.,factor = .8),
@@ -181,20 +208,23 @@ p_leg2 <- tibble(spec1 = c('indigo', 'indigo','unicolor'),
   cowplot::plot_grid(plotlist = .,
                      nrow = 1)
 
+# create sub-legend 3
 p_leg_3 <- cowplot::plot_grid(p_leg1,
                             p_leg2,
                             nrow = 1, rel_widths = c(.6, 1))
-
+# complete legend
 p_leg <- cowplot::plot_grid(
   p_leg_poptree,
   p_leg_3,
   ncol = 1,
   rel_heights = c(.2, 1))
 
+# finalize figure
 p_done <- cowplot::plot_grid(p_single, p_leg,
                              ncol = 1,
                              rel_heights = c(1, .17))
 
+# export figure
 scl <- .9
 hypo_save(plot = p_done, filename = 'figures/F5.pdf',
           width = 14*scl, height = 13*scl,
